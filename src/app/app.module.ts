5ddd303d7aa6bb56e27ee76602d5f85898e938f6
BeAmazedVariable <thienphuoc.0108@gmail.com>
2021-04-22 16:30:19 -0400

    Hello

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { ContactusComponent } from './contactus/contactus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponent,
+    ContactusComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
